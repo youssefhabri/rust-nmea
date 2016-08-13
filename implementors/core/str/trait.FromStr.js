c46a2f2ae5c31c660722a470ccac76f3fff1f32c
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2016-08-13 10:33:08 +0200

    Updated for version 0.0.2

--- implementors/core/str/trait.FromStr.js
+++ implementors/core/str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['nmea'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='https://doc.rust-lang.org/regex/regex/re_bytes/struct.Regex.html' title='regex::re_bytes::Regex'>Regex</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='https://doc.rust-lang.org/regex/regex/re_unicode/struct.Regex.html' title='regex::re_unicode::Regex'>Regex</a>",];
+implementors['nmea'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='struct' href='https://doc.rust-lang.org/regex/regex/re_unicode/struct.Regex.html' title='regex::re_unicode::Regex'>Regex</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
